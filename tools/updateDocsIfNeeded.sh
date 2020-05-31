41485b913de861d247930b0e50cf50441f9cdc35
ddio <ddioibb@gmail.com>
2020-05-31 14:46:23 +0800

    feat: add auto syncAll script fron travis ci cronjob

---
+++ tools/updateDocsIfNeeded.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+# Credit: https://www.vinaygopinath.me/blog/tech/commit-to-master-branch-on-github-using-travis-ci/
+
+setup_git() {
+  git config --global user.email "bot@summit.g0v.tw"
+  git config --global user.name "Summit Bot"
+}
+
+commit_country_json_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f assets
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Document auto update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://$GITHUB_TOKEN@github.com/ddio/summit2020.git
+  git push origin master
+}
+
+setup_git
+
+commit_country_json_files
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "Document update found. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes found. Nothing to do"
+fi
