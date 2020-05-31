63f6c8c4ef5ced052aa0b28ca3f9aa7894833ddb
leia hsu <luv.hsu@gmail.com>
2020-05-31 22:21:17 +0800

    styling index

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <div class="landing flex justify-center items-center bg-near-white">
-      <h1>{{ $t('welcome') }}</h1>
+      <img class="g0v-logo" :src="require('~/assets/images/g0v-logo.svg')" alt="" srcset="">
     </div>
     <div class="content">
-      <div>
+      <div class="article">
+        <img class="blue-circle" :src="require('~/assets/images/blue-circle.svg')" alt="" srcset="">
         <summit-markdown :content="$t('article/communityIntro')"></summit-markdown>
       </div>
-      <div>
+      <div class="article">
+        <img class="green-circle" :src="require('~/assets/images/green-circle.svg')" alt="" srcset="">
+        <summit-markdown :content="$t('article/summitAbout')"></summit-markdown>
         <summit-markdown :content="$t('article/summitIntro')"></summit-markdown>
       </div>
-      <div>
+      <div class="article">
+        <img class="blue-circle" :src="require('~/assets/images/blue-circle.svg')" alt="" srcset="">
+        <img class="green-circle" :src="require('~/assets/images/green-circle.svg')" alt="" srcset="">
         <summit-markdown :content="$t('article/summitGuidelines')"></summit-markdown>
       </div>
     </div>
@@ -21,21 +26,48 @@
 export default {}
 </script>
 <style lang="scss" scoped>
-/deep/ *{
-  color:#555555;
+/deep/ * {
+  color: #555555;
   line-height: 1.7;
 }
 .landing {
-  min-height: 50vh;
+  min-height: 90vh;
   max-height: 922px;
+  position: relative;
+  background: url('../assets/images/banner.png') center center no-repeat;
+  background-size: cover;
+  .g0v-logo{
+    width:25vw
+  }
 }
 .content {
   width: 100%;
   padding: 0 20%;
   /deep/ h1 {
-    color:#50BC83;
-    font-size: 32px;
+    color: #50bc83;
+    font-size: 1.5em;
+  }
+  /deep/ h2 {
+    font-weight: normal;
+    color: #50bc83;
+    font-size: 1.3em;
+  }
+  .article{
+    margin:200px 0;
+    position: relative;
+  }
+  .blue-circle,.green-circle{
+    position: absolute;
+    width:15vw
+  }
+  .blue-circle{
+    left:-20vw;
+    top:10vh;
+    transform:rotate(180deg);
+  }
+  .green-circle{
+    right:-20vw;
+    bottom:10vh;
   }
 }
-
 </style>
