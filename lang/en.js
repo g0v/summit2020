7da3a7f7fc01f672af84e86bb4bfa854fc84ecea
acid7820@gmail.com <acid7820@gmail.com>
2020-05-31 20:07:49 +0800

    conflict fixed

--- lang/en.js
+++ lang/en.js
@@ -17,6 +17,7 @@ export default {
   'article/coc': require('~/assets/articles/行為準則.en.md').default,
   'article/communityIntro': require('~/assets/articles/社群簡介.en.md').default,
   'article/summitIntro': require('~/assets/articles/雙年會簡介.en.md').default,
+  'article/aboutSummit': require('~/assets/articles/關於雙年會.en.md').default,
 
   // tables
   'table/roughSchedule': extractLanguageFromTable({
--- lang/zh.js
+++ lang/zh.js
@@ -16,6 +16,7 @@ export default {
   'article/cohostIntro': require('~/assets/articles/共同主辦介紹.zh.md').default,
   'article/communityIntro': require('~/assets/articles/社群簡介.zh.md').default,
   'article/summitIntro': require('~/assets/articles/雙年會簡介.zh.md').default,
+  'article/aboutSummit': require('~/assets/articles/關於雙年會.zh.md').default,
 
   // tables
   'table/roughSchedule': extractLanguageFromTable({
