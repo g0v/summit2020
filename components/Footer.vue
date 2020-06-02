81a8b946015b6181e553c7627e3462b157799188
acid7820@gmail.com <acid7820@gmail.com>
2020-06-03 00:52:39 +0800

    modify mobile css in Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,25 +1,17 @@
 <template>
-  <div class="footer flex flex-column flex-row-l justify-between items-center bb b--moon-gray shadow-1">
+  <div class="footer bb b--moon-gray shadow-1">
     <div class="icon-container">
       <nuxt-link class="text" :to="'#'">
         {{ $t('follow') }}
       </nuxt-link>
-      <nuxt-link class="g0v-icon" :to="'#'" />
-      <nuxt-link class="fa fa-facebook-f" :to="'#'">
-        {{ $t('') }}
-      </nuxt-link>
-      <nuxt-link class="fa fa-twitter" :to="'#'">
-        {{ $t('') }}
-      </nuxt-link>
+      <a class="g0v-icon" href="http://g0v.tw/en-US/"></a>
+      <a class="fa fa-facebook-f" href="https://www.facebook.com/g0v.tw/"></a>
+      <a class="fa fa-twitter" href="https://twitter.com/g0vtw"></a>
       <nuxt-link class="text" :to="'#'">
         {{ $t('collaboration') }}
       </nuxt-link>
-      <nuxt-link class="fa fa-slack" :to="'#'">
-        {{ $t('') }}
-      </nuxt-link>
-      <nuxt-link class="fa fa-github" :to="'#'">
-        {{ $t('') }}
-      </nuxt-link>
+      <a class="fa fa-slack" href="http://join.g0v.today/"></a>
+      <a class="fa fa-github" href="https://github.com/g0v/summit2020"></a>
       <nuxt-link class="fa fa-file-text" :to="'#'">
         {{ $t('') }}
       </nuxt-link>
@@ -27,9 +19,9 @@
         {{ $t('') }}
       </nuxt-link>
     </div>
-    <nuxt-link class="text" :to="'#'">
+    <span class="text" :to="'#'">
       Copyright <span class="fa fa-copyright" /> g0v Summit 2020
-    </nuxt-link>
+    </span>
   </div>
 </template>
 <i18n lang="yaml">
@@ -48,44 +40,59 @@ export default {
 </script>
 <style lang="scss" scoped>
 .footer {
-  position: fixed;
-  bottom: 0;
-  width: 100vw;
-  z-index: 9999;
-  font-family: Noto Sans CJK TC;
-  font-size: 2rem;
-  @media (max-width: 1681px) {
-    font-size: 1.5625rem;
-  }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  color: #555555;
   background-color: #fff;
-  padding: 1.5rem 2rem;
+  padding: .5rem 2rem;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
   .icon-container {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
     justify-content: space-between;
-    width: 60%;
-    @media (max-width: 950px) {
+    width: 50%;
+    @media (max-width: 960px) {
+      width: 70%;
+      margin-bottom: 8px;
+    }
+    @media (max-width: 680px) {
       width: 100%;
     }
     .g0v-icon {
       background-image: url('../assets/images/scene_16.svg');
       background-size: contain;
       background-repeat: no-repeat;
-      height: 42px;
+      background-position-x: center;
+      height: 15px;
       width: 3em;
     }
   }
   a {
-    font-size: 2rem;
     flex-shrink: 0;
-    @media (max-width: 1681px) {
-      font-size: 1.5625rem;
-    }
     color: #50BC83;
-    text-decoration: none;
     display: inline-block;
     text-align: center;
+    @media (max-width: 540px) {
+      margin-top: 8px;
+      width: 20%;
+      &:nth-child(1) {
+        width: 25%;
+      }
+      &:nth-child(2) {
+        width: 25%;
+      }
+      &:nth-child(3) {
+        width: 25%;
+      }
+      &:nth-child(4) {
+        width: 25%;
+      }
+    }
   }
   .text {
     color: #555555;
