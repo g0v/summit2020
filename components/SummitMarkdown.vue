e3efd0733489d5405ae926dc56ec9f6ef1954571
ddio <ddioibb@gmail.com>
2020-06-02 11:31:37 +0800

    feat: default ignore h1 from markdown & remove duplicated content

--- components/SummitMarkdown.vue
+++ components/SummitMarkdown.vue
@@ -17,17 +17,32 @@ export default {
     content: {
       type: String,
       required: true
+    },
+    hideH1: {
+      type: Boolean,
+      default: true
     }
   },
   computed: {
     cleanMd () {
       const meta = yamlFront.loadFront(this.content)
-      const content = meta.__content || ''
+      let content = meta.__content || ''
+
       // remove all quote as it's comment
-      return content
+      content = content
         .split('\n')
         .filter(line => !line.startsWith('>'))
         .join('\n')
+
+      if (this.hideH1) {
+        content = content
+          // remove atx format H1, aka `# This is H1`
+          .replace(/\n# [^\n]+\n/g, '\n')
+          // remove Setext format H1, aka `This is H1\n======`
+          .replace(/\n[^\n]+\n=+\n/g, '\n')
+      }
+
+      return content
     }
   }
 }
