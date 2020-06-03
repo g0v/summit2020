679f9ea819be7fa73c1791e2b027bd22a6031ba6
leia hsu <luv.hsu@gmail.com>
2020-06-04 02:23:48 +0800

    add timeline

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,48 @@
       </div>
     </div>
     <div class="time-container">
-      <img class="time" :src="require('~/assets/images/timeline.svg')">
+      <div class="timebar">
+        <div class="circle-container top one-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="https://summit.g0v.tw/2012/">2012 summit</a>
+        </div>
+        <div class="circle-container bottom two-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="https://summit.g0v.tw/2014/">2014 summit</a>
+        </div>
+        <div class="circle-container top three-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="https://summit.g0v.tw/2016/">2016 summit</a>
+        </div>
+        <div class="circle-container bottom four-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="https://summit.g0v.tw/2018/">2018 summit</a>
+        </div>
+        <div class="circle-container top five-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="https://summit.g0v.tw/2020/">2020 summit</a>
+        </div>
+        <div class="circle-container bottom six-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="http://">即日起至 6/4 投稿</a>
+        </div>
+        <div class="circle-container top seven-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="http://">即日起至 9/1</a>
+        </div>
+        <div class="circle-container bottom eight-point">
+          <div class="circle"></div>
+          <div class="circle-line"></div>
+          <a href="http://">12/03 ~ 12/6</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -30,15 +71,110 @@ export default {}
 </script>
 <style lang="scss" scoped>
 .time-container {
-  background-color: #50BC83;
+  background-color: #50bc83;
   padding: 20px;
   text-align: center;
   margin-top: 120px;
-  .time {
-    display: block;
-    width: 100%;
-    height: auto;
-    max-height: 400px;
+  min-height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .timebar{
+    height: 10px;
+    width: 90vw;
+    background-color: #555555;
+    position: relative;
+    @media (max-width: 800px) {
+      height: 90vh;
+      width: 10px;
+    }
+    &:before, &:after{
+      content:'';
+      position: absolute;
+      background-color: #555555;
+      height: 10px;
+      width: 10px;
+      border-radius:100%;
+      top:0;
+    }
+    &:before{
+      left:-5px;
+      @media (max-width: 800px) {
+        left:0;
+        top:-5px;
+      }
+    }
+    &:after{
+      right:-5px;
+      @media (max-width: 800px) {
+        top:auto;
+        right:0;
+        bottom:-5px;
+      }
+    }
+    .circle-container{
+      position: absolute;
+      display: flex;
+      align-items: center;
+      .circle{
+        width: 40px;
+        height: 40px;
+        background-color: #fff;
+        border-radius:100%;
+      }
+      .circle-line{
+          height: 100px;
+          width: 2px;
+          background-color: #fff;
+      }
+      a{
+        position: absolute;
+        width: 200px;
+        color:white;
+        text-decoration: none;
+        font-size: 1.5rem;
+      }
+    }
+    .circle-container.bottom{
+      flex-direction: column;
+      top: -150%;
+      a{
+        bottom: -35px;
+        left: -0;
+      }
+    }
+    .circle-container.top{
+      flex-direction: column-reverse;
+      bottom: -150%;
+      a{
+        top: -35px;
+        left: -0;
+      }
+    }
+    .circle-container.one-point{
+      left:7%;
+    }
+    .circle-container.two-point{
+      left:18%;
+    }
+    .circle-container.three-point{
+      left:29%;
+    }
+    .circle-container.four-point{
+      left:42%;
+    }
+    .circle-container.five-point{
+      left:53%;
+    }
+    .circle-container.six-point{
+      left:65%;
+    }
+    .circle-container.seven-point{
+      left:75%;
+    }
+    .circle-container.eight-point{
+      left:85%;
+    }
   }
 }
 .landing {
@@ -87,6 +223,120 @@ export default {}
   .green-circle {
     right: -20vw;
     bottom: 20vh;
+  }
+}
+
+@media (max-width: 800px) {
+  .landing .g0v-logo{
+    width: 70vw;
+  }
+  .time-container{
+    .timebar{
+    height: 90vh;
+    width: 10px;
+    &:before{
+      left:0;
+      top:-5px;
+    }
+    &:after{
+      top:auto;
+      right:0;
+      bottom:-5px;
+    }
+    .circle-container{
+      .circle-line{
+        height: 2px;
+        width: 100px;
+      }
+      .circle{
+        width: 20px;
+        height: 20px;
+      }
+      &.top{
+        flex-direction: row;
+        bottom: auto;
+        left:-5px;
+        a{
+          left: 20px;
+        }
+      }
+      &.bottom{
+        flex-direction: row-reverse;
+        bottom: auto;
+        left:-105px;
+        a{
+          right: auto;
+          left: -100px;
+        }
+      }
+      &.one-point{
+        top:5%;
+      }
+      &.two-point{
+        top:17%;
+      }
+      &.three-point{
+        top:29%;
+      }
+      &.four-point{
+        top:40%;
+      }
+      &.five-point{
+        top:52%;
+      }
+      &.six-point{
+        top:67%;
+      }
+      &.seven-point{
+        top:79%;
+      }
+      &.eight-point{
+        top:90%;
+      }
+    }
+    // .circle-container{
+    //   position: absolute;
+    //   .circle{
+    //     width: 40px;
+    //     height: 40px;
+    //     background-color: #fff;
+    //     border-radius:100%;
+    //   }
+    //   .circle-line{
+    //       height: 100px;
+    //       width: 2px;
+    //       background-color: #fff;
+    //   }
+    //   a{
+    //     position: absolute;
+    //     width: 200px;
+    //     color:white;
+    //     text-decoration: none;
+    //     font-size: 1.5rem;
+    //   }
+    // }
+    // .circle-container.bottom{
+    //   display: flex;
+    //   flex-direction: column;
+    //   align-items: center;
+    //   top: -150%;
+    //   a{
+    //     bottom: -35px;
+    //     left: -0;
+    //   }
+    // }
+    // .circle-container.top{
+    //   display: flex;
+    //   flex-direction: column-reverse;
+    //   align-items: center;
+    //   bottom: -150%;
+    //   a{
+    //     top: -35px;
+    //     left: -0;
+    //   }
+    // }
+
+  }
   }
 }
 </style>
