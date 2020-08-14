4cfa54f4bc42fd4e3ba79d98197bc819465577c7
acid7820@gmail.com <acid7820@gmail.com>
2020-08-14 10:00:49 +0800

    modify UI v1 to v2 in Timeline

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -31,20 +31,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$w: 0.5rem;
+$w: 0.4rem;
 $d: 2rem;
-
+$islandScale: 0.65;
+$islandWidth: 760px;
+$islandHeight: 142px;
 .timeline {
   position: relative;
-  background-color: $green;
-  color: white;
+  background-color: #F7F7F7;
+  color: $gray;
   line-height: 1.5;
   margin: 2rem 0;
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    z-index: 2;
+    width: $islandWidth * $islandScale;
+    height: $islandHeight * $islandScale;
+    top: -125px * $islandScale;
+    left: 0px;
+    background-image: url('../assets/images/v2/island-5.svg');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  @media (min-width: 801px) {
+    &::after {
+      width: $islandWidth;
+      height: $islandHeight;
+      top: -125px;
+      left:  -$islandWidth / 2;
+      right: 0;
+      margin: auto;
+    }
+  }
   &:before {
     content: '';
     display: block;
     position: absolute;
-    background-color: $gray;
+    background-color: $blue-1;
   }
   @media (max-width: 800px) {
     padding: 2rem 2rem;
@@ -94,8 +119,17 @@ $d: 2rem;
         $ds: $d / 5 * 3;
         width: $ds;
         height: $ds;
-        transform: translate(($d - $ds) / 2, ($d - $ds) / 2);
+        @media (max-width: 800px) {
+          transform: translate(($d - $ds) / 2, ($d - $ds) / 2);
+        }
       }
+    }
+  }
+  > .point {
+    > .circle {
+      border: 1px solid $blue-1;
+      background-color: white;
+      border-radius: 50%;
     }
   }
   @media (max-width: 800px) {
@@ -108,8 +142,6 @@ $d: 2rem;
         left: 0;
         width: $d;
         height: $d;
-        border-radius: 50%;
-        background-color: white;
       }
       > .label {
         margin-left: $d*1.25;
@@ -126,8 +158,6 @@ $d: 2rem;
     > .circle {
       width: $d;
       height: $d;
-      border-radius: 50%;
-      background-color: white;
     }
     > .label {
       position: absolute;
