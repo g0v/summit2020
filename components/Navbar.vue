d3016fedd994aee2ca62c7e35a7f32ad22aca104
ddio <ddioibb@gmail.com>
2020-09-16 23:00:12 +0800

    chroe: simplify menu usage

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,15 +7,7 @@
       nuxt-link.navbar__logo.flex-auto.flex-none-l.tc(:to="localePath('/')")
         img.h-100(src="~/assets/images/v2/logo-singleline.svg")
       .dn.flex-l.items-center
-        template(v-for="menu in menuList")
-          component.navbar__item.dim.pa2.mr3(
-            :is="menu.isExt ? 'ext-link' : 'nuxt-link'"
-            :key="menu.key"
-            :to="genLink(menu)"
-          )
-            | {{ $t(menu.key) }}
-        nuxt-link.navbar__lang.pa2(:to="switchLocalePath(isZh ? 'en' : 'zh')")
-          | {{$t('lang')}}
+        summit-menu
     .mobilemenu.fixed.top-0.db.dn-l.z-999(v-show="isMobileMenuVisible")
       .mobilemenu__wrapper.vh-100.overflow-y-auto.bg-black-50(
         @click="closeMobileMenu"
@@ -23,83 +15,21 @@
         .mobilemenu__content.vh-100.w-80.ph3.pv1.flex.flex-column.justify-between(
           @click.stop
         )
-          div.flex.flex-column.flex-auto
-            template(v-for="menu in menuList")
-              component.navbar__item.dim.ph2.pv3.bb.b--moon-gray(
-                :is="menu.isExt ? 'ext-link' : 'nuxt-link'"
-                :key="menu.key"
-                :to="genLink(menu)"
-                @click.native="closeMobileMenu"
-              )
-                | {{ $t(menu.key) }}
-          nuxt-link.navbar__lang.ph2.pv3.bt.b--moon-gray(
-            :to="switchLocalePath(isZh ? 'en' : 'zh')"
-            @click.native="closeMobileMenu"
-          )
-            | {{$t('lang')}}
+          summit-menu(@click.native="closeMobileMenu")
 </template>
-<i18n lang="yaml">
-en:
-  lang: '華語'
-  speakers: 'Speakers'
-  cfp: 'Get Latest Proposals'
-  agenda: 'Agenda'
-  partners: 'Partners'
-  cosponsor: 'Cosponsor'
-  transport: 'Transportation'
-  live: 'Live'
-  feed: 'Feed'
-  staff: 'Staff'
-  registration: 'Registration'
-zh:
-  lang: 'English'
-  speakers: '講者'
-  cfp: '看議程投稿'
-  agenda: '議程'
-  partners: '合作夥伴'
-  cosponsor: '共同主辦'
-  transport: '交通'
-  live: '直播'
-  feed: '社群動態'
-  staff: '工作人員'
-  registration: '立即購票'
-</i18n>
 <script>
-import ExtLink from '~/components/ExtLink'
+import SummitMenu from './SummitMenu'
 
 export default {
   components: {
-    ExtLink
+    SummitMenu
   },
   data () {
     return {
-      isMobileMenuVisible: false,
-      menuList: [
-        { key: 'transport', url: '/transport' },
-        { key: 'cfp', url: 'https://propose.summit2020.g0v.tw/proposal-list', isExt: true }
-        // { key: 'speakers', url: '/speakers' },
-        // { key: 'agenda', url: '/agenda' },
-        // { key: 'partners', url: '/partners' },
-        // { key: 'transport', url: '/transport' },
-        // { key: 'live', url: 'https://some.live.url.com', isExt: true },
-        // { key: 'feed', url: 'https://some.feed.url.com', isExt: true },
-        // { key: 'staff', url: '/staff' },
-        // { key: 'registration', url: 'https://xxx.kktix.com', isExt: true }
-      ]
-    }
-  },
-  computed: {
-    isZh () {
-      return this.$i18n.locale === 'zh'
+      isMobileMenuVisible: false
     }
   },
   methods: {
-    genLink (menu) {
-      if (menu.isExt) {
-        return menu.url
-      }
-      return this.localePath(menu.url)
-    },
     openMobileMenu () {
       this.isMobileMenuVisible = true
     },
@@ -123,13 +53,6 @@ export default {
     @include large-screen {
       height: 46px;
     }
-  }
-  &__item {
-    color: #6e6e6e;
-    text-decoration: none;
-  }
-  &__lang {
-    color: #f779ee;
   }
 }
 
---
+++ components/SummitMenu.vue
@@ -0,0 +1,88 @@
+<template lang="pug">
+.menu.flex.flex-column.flex-row-l.vh-100.h-auto-l
+  .flex-auto.flex-none-l
+    template(v-for="menu in menuList")
+      component.menu__item.dim.ph2.pv3.pv2-l.mr3-l.db.dib-l(
+        :is="menu.isExt ? 'ext-link' : 'nuxt-link'"
+        :key="menu.key"
+        :to="genLink(menu)"
+      )
+        | {{ $t(menu.key) }}
+  nuxt-link.menu__lang.ph2.pv3.pv2-l(:to="switchLocalePath(isZh ? 'en' : 'zh')")
+    | {{$t('lang')}}
+</template>
+<i18n lang="yaml">
+en:
+  lang: '華語'
+  speakers: 'Speakers'
+  cfp: 'Get Latest Proposals'
+  agenda: 'Agenda'
+  partners: 'Partners'
+  cosponsor: 'Cosponsor'
+  transport: 'Transportation'
+  live: 'Live'
+  feed: 'Feed'
+  staff: 'Staff'
+  registration: 'Registration'
+zh:
+  lang: 'English'
+  speakers: '講者'
+  cfp: '看議程投稿'
+  agenda: '議程'
+  partners: '合作夥伴'
+  cosponsor: '共同主辦'
+  transport: '交通'
+  live: '直播'
+  feed: '社群動態'
+  staff: '工作人員'
+  registration: '立即購票'
+</i18n>
+<script>
+import ExtLink from '~/components/ExtLink'
+
+export default {
+  components: {
+    ExtLink
+  },
+  data () {
+    return {
+      menuList: [
+        { key: 'transport', url: '/transport' },
+        { key: 'cfp', url: 'https://propose.summit2020.g0v.tw/proposal-list', isExt: true }
+        // { key: 'speakers', url: '/speakers' },
+        // { key: 'agenda', url: '/agenda' },
+        // { key: 'partners', url: '/partners' },
+        // { key: 'transport', url: '/transport' },
+        // { key: 'live', url: 'https://some.live.url.com', isExt: true },
+        // { key: 'feed', url: 'https://some.feed.url.com', isExt: true },
+        // { key: 'staff', url: '/staff' },
+        // { key: 'registration', url: 'https://xxx.kktix.com', isExt: true }
+      ]
+    }
+  },
+  computed: {
+    isZh () {
+      return this.$i18n.locale === 'zh'
+    }
+  },
+  methods: {
+    genLink (menu) {
+      if (menu.isExt) {
+        return menu.url
+      }
+      return this.localePath(menu.url)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.menu {
+  &__item {
+    color: #6e6e6e;
+    text-decoration: none;
+  }
+  &__lang {
+    color: #f779ee;
+  }
+}
+</style>
